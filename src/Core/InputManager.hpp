c03dd2c94cb0b732e2721b894a838c7d20b36278
Maciej Deroń <maciejderon@icloud.com>
2025-06-21 09:02:40 +0200

    Reuse meshes and cache collision boxes

--- src/Core/InputManager.hpp
+++ src/Core/InputManager.hpp
@@ -3,6 +3,10 @@
 #ifndef INPUTMANAGER_HPP
 #define INPUTMANAGER_HPP
 
+#include <vector>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
 namespace Core {
     class InputManager
     {
@@ -14,8 +18,15 @@ namespace Core {
         static double GetMouseX();
         static double GetMouseY();
 
+        static void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
+        static void MouseButtonCallback(GLFWwindow* window, int button, int action, int mods);
+        static void CursorPosCallback(GLFWwindow* window, double xpos, double ypos);
+
     private:
         static double _mouseX, _mouseY;
+        static bool _keys[GLFW_KEY_LAST + 1];
+        static bool _mouseButtons[GLFW_MOUSE_BUTTON_LAST + 1];
+        static std::vector<int> _activeKeys;
     };
 }
 
